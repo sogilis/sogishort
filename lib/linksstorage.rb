93f95ee8ba35e2a0e913e0479c76e135cbbc5daa
Yves Brissaud <yves@sogilis.com>
2014-03-25 15:24:38 +0100

    store links under links/

--- lib/linksstorage.rb
+++ lib/linksstorage.rb
@@ -2,25 +2,28 @@ require_relative 'git'
 
 class LinksStorage < Git
 
+  LINKS_PATH = 'links'
+
   # @param [String] link
   def write_link(link)
     hash = nil
     write "add link" do |index|
       oid = write_blob(link)
       hash = oid[0..4]
-      index.add blob_with_oid(oid, hash)
+      index.add blob_with_oid(oid, link_path hash)
     end
     hash
   end
 
   # @param [String] hash
   def get_url(hash)
-    read_blob hash
+    read_blob link_path hash
   end
 
   def get_links
     result = []
-    get_tree.each do |entry|
+    links = read_tree LINKS_PATH
+    links.each do |entry|
       url = @repository.read(entry[:oid]).data
       hash = entry[:name]
       result << {:url => url, :hash => hash}
@@ -29,6 +32,10 @@ class LinksStorage < Git
   end
 
 private
+
+  def link_path(path)
+    "#{LINKS_PATH}/#{path}"
+  end
 
   # @param [String] oid
   # @param [String] path
